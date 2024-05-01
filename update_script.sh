37e4fe6b91d1b8fe26a8878839585f671bb6d9b9
Dhiaeddine Jraidi <130515774+Dhiaeddine-Jraidi@users.noreply.github.com>
2024-05-01 05:55:02 +0100

    .

--- update_script.sh
+++ update_script.sh
@@ -5,12 +5,12 @@ SOURCE="/tmp/new_version/"
 DESTINATION="/home/ubuntu/promethiusv8/"
 
 sudo supervisorctl -c /etc/supervisor/supervisord.conf stop all
-cd ~/promethiusv8
+cd $DESTINATION
 sudo rm -rf __pycache__
 sudo find . -type f ! \( -name 'open_trades.csv' -o -name 'syfer_PendingTrades.json' -o -name 'temporary_finished_trade.csv' -o -name 'update_script.py' -o -name 'update_script.sh' \) -exec rm -v {} +
-sudo mkdir -p /tmp/new_version
-cd /tmp/new_version
+sudo mkdir -p $SOURCE
+cd $SOURCE
 sudo git clone https://github.com/Dhiaeddine-Jraidi/promethiusv8.git .
 sudo $RSYNC -av --exclude='update_script.sh' --exclude='update_script.py' $SOURCE $DESTINATION
-sudo rm -rf /tmp/new_version
+sudo rm -rf $SOURCE
 sudo supervisorctl -c /etc/supervisor/supervisord.conf start all
