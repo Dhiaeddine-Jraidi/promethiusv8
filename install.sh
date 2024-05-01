159449be336cbd98b6f952c8db6e31c7e3ecd94d
Dhiaeddine Jraidi <130515774+Dhiaeddine-Jraidi@users.noreply.github.com>
2024-05-01 03:58:08 +0100

    .

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo service supervisor start
 git clone https://github.com/Dhiaeddine-Jraidi/promethiusv8.git
 sudo chmod a+rwx promethiusv8
 cd promethiusv8
+sudo chmod +x update_script.sh
 sudo pip install -r requirements.txt
 sudo cp ~/promethiusv8/promethius.conf /etc/supervisor/conf.d/promethius.conf
 sudo supervisorctl -c /etc/supervisor/supervisord.conf
