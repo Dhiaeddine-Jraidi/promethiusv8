78947662649258d0558988ab60a18e7ff743625a
Dhiaeddine Jraidi <130515774+Dhiaeddine-Jraidi@users.noreply.github.com>
2024-05-02 17:35:13 +0100

    .

--- update_script.sh
+++ update_script.sh
@@ -7,7 +7,7 @@ DESTINATION="/home/ubuntu/promethiusv8/"
 sudo supervisorctl -c /etc/supervisor/supervisord.conf stop all
 cd $DESTINATION
 sudo rm -rf __pycache__
-sudo find . -type f ! \( -name 'open_trades.csv' -o -name 'syfer_PendingTrades.json' -o -name 'temporary_finished_trade.csv' -o -name 'update_script.sh' \) -exec rm -v {} +
+sudo find . -type f ! \( -name 'main_logger.txt' -o -name 'telegram_handler_logger.txt' -o -name 'tracking_opened_trades_logger.txt' -o -name 'open_trades.csv' -o -name 'syfer_PendingTrades.json' -o -name 'temporary_finished_trade.csv' -o -name 'update_script.sh' \) -exec rm -v {} +
 sudo mkdir -p $SOURCE
 cd $SOURCE
 sudo git clone https://github.com/Dhiaeddine-Jraidi/promethiusv8.git .
