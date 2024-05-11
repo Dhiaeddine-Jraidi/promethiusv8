a1a1101ba1cd7e6a3211d872d02eb3574afa495c
Dhiaeddine Jraidi <130515774+Dhiaeddine-Jraidi@users.noreply.github.com>
2024-05-11 08:33:56 +0100

    .

--- update_script.sh
+++ update_script.sh
@@ -3,8 +3,8 @@
 RSYNC="/usr/bin/rsync"
 SOURCE="/tmp/promethiusv8_new_version/"
 DESTINATION="/home/ubuntu/promethiusv8/"
-DOWNLOAD_DIRECTORY="/home/ubuntu/promethiusv8/files/download"
-LOGGER_DIRECTORY="/home/ubuntu/promethiusv8/files/logger"
+DOWNLOAD_DIRECTORY="/home/ubuntu/promethiusv8/files/download/"
+LOGGER_DIRECTORY="/home/ubuntu/promethiusv8/files/logger/"
 
 
 sudo supervisorctl -c /etc/supervisor/supervisord.conf stop all
@@ -16,9 +16,6 @@ cd $SOURCE
 sudo git clone https://github.com/Dhiaeddine-Jraidi/promethiusv8.git .
 sudo $RSYNC -av --exclude='update_script.sh' $SOURCE $DESTINATION
 cd $DESTINATION
-sudo pip install -r requirements.txt
-sudo cp -f /home/ubuntu/promethiusv8/promethius_runner.conf /etc/supervisor/conf.d/promethius_runner.conf
-sudo rm -rf $SOURCE
 
 if [ ! -d "$DOWNLOAD_DIRECTORY" ]; then
     sudo mkdir -p "$DOWNLOAD_DIRECTORY"
@@ -29,6 +26,9 @@ if [ ! -d "$LOGGER_DIRECTORY" ]; then
     sudo mkdir -p "$LOGGER_DIRECTORY"
 fi
 
+sudo pip install -r requirements.txt
+sudo cp -f /home/ubuntu/promethiusv8/promethius_runner.conf /etc/supervisor/conf.d/promethius_runner.conf
+sudo rm -rf $SOURCE
 sudo supervisorctl -c /etc/supervisor/supervisord.conf reread
 sudo supervisorctl -c /etc/supervisor/supervisord.conf update
 sudo supervisorctl -c /etc/supervisor/supervisord.conf start all
